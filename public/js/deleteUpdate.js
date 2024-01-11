8a4b38cb77b5cb9c94f6c8f220f472ee0c704e52
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-11 01:31:07 -0500

    adds code to deleteUpdate.js

---
+++ public/js/deleteUpdate.js
@@ -0,0 +1,38 @@
+document.querySelector("#update").addEventListener("click",event=>{
+    event.preventDefault();
+    const blogId = document.querySelector("#hiddenBlogId").value;
+    const editBlog = {
+        title:document.querySelector("#editedTitle").value,
+        content:document.querySelector("#editedContent").value,
+    }
+    
+    fetch((`/api/blogs/${blogId}`),{
+        method:"PUT",
+        body:JSON.stringify(editBlog),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            console.log("Updated Blog")
+            location.href="/dashboard"
+        } else {
+            alert("Try again!")
+        }
+    })
+})
+
+document.querySelector("#delete").addEventListener("click",event=>{
+    event.preventDefault();
+    const blogId = document.querySelector("#hiddenBlogId").value;
+    fetch((`/api/blogs/${blogId}`),{
+        method:"DELETE",
+    }).then(res=>{
+        if(res.ok){
+            console.log("Deleted blog")
+            location.href="/dashboard"
+        } else {
+            alert("Try again!")
+        }
+    })
+})
