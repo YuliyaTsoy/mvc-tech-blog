8defc7ca413bb620cb53b610b0e171b0ecac0b7e
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-10 22:45:56 -0500

    adds code to server.js file

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+const { User, Blog, Comment } = require("./models");
+
+const sess = {
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: {
+    // 24 hours
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Now listening");
+  });
+});
